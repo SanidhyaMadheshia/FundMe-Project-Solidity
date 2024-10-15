da2cce78e84a4385aaf04e435db35abbb9d97b17
SanidhyaMadheshia <sanidhyamadheshia@gmail.com>
2024-10-15 19:27:55 +0530

    deployable contractand done make file integration and lot more stuffs

---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test,console} from "forge-std/Test.sol";
+
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+contract FundMeTest is Test {
+    // uint256 numbe=1;
+    FundMe fundMe;
+     
+     address USER = makeAddr("user");
+     uint256 constant SEND_VALUE = 0.1 ether;
+     uint256 constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        // numbe=2;
+        // fundMe  = new FundMe();//instead of passing the adress two times
+
+        DeployFundMe  deployFundMe = new DeployFundMe();
+
+        fundMe=deployFundMe.run();
+
+        vm.deal(USER,STARTING_BALANCE) ; 
+        // give the starting balance to the user 
+
+
+
+    }
+
+    function testDemo() public {
+        // assertEq(numbe,2);
+        fundMe.MINIMUM_USD();
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+
+
+    }
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+
+        // assertEq(fundMe.i_owner(),address(this));
+        assertEq(fundMe.i_owner(),msg.sender);
+
+    }
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+
+        assertEq(version, 4);
+    }
+
+    // function testFundFailWithoutEnoughEth() public {
+    //     vm.expectRevert(); // ---> means that the next line should revert !
+    //     // example = fundme.fund();
+
+         
+
+    // }
+
+    function testFundUpdatesFundedDataStructures() public {
+        vm.prank(USER);//the next Tx will be send by user;
+        fundMe.fund{value : SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+
+    }
+    //to avoid writing everytime the vm.prank we should  use modifiers;
+
+    modifier funded() {
+        vm.prank(USER); // next trnx will be send by USER;
+
+        fundMe.fund{value : SEND_VALUE}();
+        _;
+    }
+
+
+    function testAddsFunderToArrayOfFunders() public funded { // used modifiers 
+
+
+
+        address funder = fundMe.getFunder(0);
+
+        assertEq(funder,USER);
+
+
+    }
+    function testOnlyUserCanWithdraw() public  funded{// the test word should always be in front of function world of test
+        
+
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
+        
+    }
+    function testWithDrawWithSingleFunder() public funded {
+        // Arrange  
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act 
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+
+
+        // Assert 
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256  endingFundMeBalance = address(fundMe).balance;
+
+        assertEq(endingFundMeBalance, 0);
+        assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
+    }
+
+
+    function testWithDrawWithMultipleFunders() public funded {
+        //arrange 
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+
+        for(uint160 i = startingFunderIndex;i<numberOfFunders;i++) {
+            // vm.prank new address
+            // vm.deal new address 
+            hoax(address(i) , SEND_VALUE);
+            fundMe.fund{value : SEND_VALUE}();
+
+        }
+
+        //act 
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+        assertEq(address(fundMe).balance, 0);
+        assertEq(startingFundMeBalance + startingOwnerBalance, fundMe.getOwner().balance);
+
+
+        
+    }
+
+}
+
+
+
+
+
